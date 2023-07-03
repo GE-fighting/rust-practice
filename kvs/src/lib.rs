d5be1225ffe2d2f6eb85a8be16737c80d0719543
zhangjun <1501790343@qq.com>
2023-07-03 11:41:09 +0800

    project2 setup

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,4 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
+
+
+#[derive(Debug)]
+pub enum Error {
+   
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub struct KvStore {
@@ -12,15 +20,21 @@ impl KvStore {
         KvStore { store: mapStore }
     }
     ///set a key/value pair in the store
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<bool> {
         self.store.insert(key, value);
+        Ok(true)
     }
     ///get a key/value pair from the store
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<String> {
+       let res_string =  self.store.get(&key).cloned().unwrap();
+       Ok(res_string)
     }
     ///remove a key/value pair from the
     pub fn remove(&mut self, key: String) -> bool {
         self.store.remove(&key).is_some()
+    }
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore>{
+        let kv =  KvStore::new();
+        return Ok(kv);
     }
 }
